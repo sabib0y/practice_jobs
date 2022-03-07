0d22f4f177b049b4a6ee3d469985762130d590ec
paul idemudia <paul.idemudia@nss.nhs.scot>
2022-03-07 12:57:16 +0000

    made changes to search bar

--- pages/components/SearchBar.tsx
+++ pages/components/SearchBar.tsx
@@ -14,72 +14,76 @@ interface Props {
 }
 
 
-const SearchBar: React.FC<Props> = ({searchParamss,  searchParams, onInputChange }) => {
+const SearchBar: React.FC<Props> = ({ searchParamss, searchParams, onInputChange }) => {
 
     const handleClick = () => {
-        console.log('ping');
         searchParamss(searchParams)
     }
 
-    return (
-        <div className={styles.search_bar}>
-            <TextField
-                hiddenLabel
-                id="filled-hidden-label-small"
-                placeholder='Search for a Vacancy'
-                variant="filled"
-                size="small"
-                onChange={onInputChange}
-                className={styles.text_field}
-                value={searchParams.title}
-                name='title'
-            />
-
-            <FormControl sx={{ m: 1, minWidth: 120 }} className={styles.region_field}>
-                <Select
+    if (searchParams) {
+        return (
+            <div className={styles.search_bar}>
+                <TextField
+                    hiddenLabel
+                    id="filled-hidden-label-small"
+                    placeholder='Search for a Vacancy'
+                    variant="filled"
+                    size="small"
                     onChange={onInputChange}
-                    displayEmpty
-                    inputProps={{ 'aria-label': 'Without label' }}
-                    name='nhsBoard'
-                    value={searchParams.nhsBoard}
+                    className={styles.text_field}
+                    value={searchParams.title}
+                    name='title'
+                />
+
+                <FormControl sx={{ m: 1, minWidth: 120 }} className={styles.region_field}>
+                    <Select
+                        onChange={onInputChange}
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Without label' }}
+                        name='nhsBoard'
+                        value={searchParams.nhsBoard}
+                    >
+                        <MenuItem value="">
+                            <em>Region</em>
+                        </MenuItem>
+                        {regions.map(({ label, value }, i) => (
+                            <MenuItem value={value} key={i}>{label}</MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+
+                <FormControl sx={{ m: 1, minWidth: 120 }} className={styles.category_field}>
+                    <Select
+                        onChange={onInputChange}
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Without label' }}
+                        name='jobFamily'
+                        value={searchParams.jobFamily}
+
+                    >
+                        <MenuItem value="">
+                            <em>Category</em>
+                        </MenuItem>
+                        {categories.map(({ label, value }, i) => (
+                            <MenuItem value={value} key={i}>{label}</MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+
+                <Button
+                    variant="contained"
+                    onClick={handleClick}
+                    className={styles.search_btn}
                 >
-                    <MenuItem value="">
-                        <em>Region</em>
-                    </MenuItem>
-                    {regions.map(({ label, value }, i) => (
-                        <MenuItem value={value} key={i}>{label}</MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
+                    Search <span className={styles.btn_icon} />
+                </Button>
 
-            <FormControl sx={{ m: 1, minWidth: 120 }} className={styles.category_field}>
-                <Select
-                    onChange={onInputChange}
-                    displayEmpty
-                    inputProps={{ 'aria-label': 'Without label' }}
-                    name='jobFamily'
-                    value={searchParams.jobFamily}
+            </div>
+        )
+    } else {
+        return null
+    }
 
-                >
-                    <MenuItem value="">
-                        <em>Category</em>
-                    </MenuItem>
-                    {categories.map(({ label, value }, i) => (
-                        <MenuItem value={value} key={i}>{label}</MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-
-            <Button
-                variant="contained"
-                onClick={handleClick}
-                className={styles.search_btn}
-            >
-                Search <span className={styles.btn_icon} />
-            </Button>
-
-        </div>
-    )
 }
 
 export default SearchBar
